9ba349c2c1c1256794aa2e8cfa315a1065ebdfc3
ShahJabir <shahjabirofficial@gmail.com>
2026-03-13 23:04:04 +0600

    feat(http): implement file serving and routing in Backlia framework

--- 02-core-modules/07-http/example/simple-backend-framwork/backlia.js
+++ 02-core-modules/07-http/example/simple-backend-framwork/backlia.js
@@ -1,12 +1,50 @@
 import http from "node:http";
+import fs from "node:fs/promises";
+import { pipeline } from "node:stream/promises";
 
 class Backlia {
   constructor() {
-    this.server = http.createServer();
+    this.routes = {};
+    this.server = http.createServer(this.handleRequest.bind(this));
   }
-  listen = (HOST = "127.0.0.1", PORT, Callback) => {
-    this.server.listen(PORT, HOST, Callback);
-  };
+
+  async handleRequest(req, res) {
+    /* 1. Decorate response FIRST */
+    res.sendFile = async (filePath, mime) => {
+      let fileHandle;
+      try {
+        res.setHeader("Content-Type", mime);
+        fileHandle = await fs.open(filePath, "r");
+        await pipeline(fileHandle.createReadStream(), res);
+      } catch (err) {
+        res.statusCode = 500;
+        res.end("Failed to send file");
+      } finally {
+        await fileHandle?.close();
+      }
+    };
+
+    /* 2. Route resolution */
+    const key = req.method.toLowerCase() + req.url;
+    const route = this.routes[key];
+
+    if (!route) {
+      res.statusCode = 404;
+      res.end("Not Found");
+      return;
+    }
+
+    /* 3. Execute route */
+    await route(req, res);
+  }
+
+  route(method, url, handler) {
+    this.routes[method.toLowerCase() + url] = handler;
+  }
+
+  listen(host, port, callback) {
+    this.server.listen(port, host, callback);
+  }
 }
 
 export default Backlia;
--- 02-core-modules/07-http/example/simple-backend-framwork/src/main.js
+++ 02-core-modules/07-http/example/simple-backend-framwork/src/main.js
@@ -2,6 +2,18 @@ import Backlia from "./../backlia.js";
 
 const app = new Backlia();
 
+app.route("GET", "/", async (_, res) => {
+  await res.sendFile("./public/index.html", "text/html");
+});
+
+app.route("GET", "/style.css", async (_, res) => {
+  await res.sendFile("./public/style.css", "text/css");
+});
+
+app.route("GET", "/script.js", async (_, res) => {
+  await res.sendFile("./public/script.js", "application/javascript");
+});
+
 app.listen("127.0.0.2", "8230", () => {
   console.log("Server connected on http://127.0.0.2:8230");
 });
