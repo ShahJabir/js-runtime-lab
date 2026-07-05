fdf1471f00edd5075b65ddc430b147e1d97c0e0b
ShahJabir <shahjabirofficial@gmail.com>
2026-07-05 21:00:29 +0600

    feat: implement Router for navigation and integrate with app initialization

--- projects/coffeemasters-vanilla/app.js
+++ projects/coffeemasters-vanilla/app.js
@@ -1,9 +1,12 @@
 import { Store } from "./Service/Store.js";
 import { loadData } from "./Service/Menu.js";
+import { Router } from "./Service/Router.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 window.addEventListener("DOMContentLoaded", () => {
   loadData();
+  app.router.init();
 });
